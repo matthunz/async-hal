e9df649e205209b8bdd39edc0770c002697c4cdb
Matt Hunzinger <matthunz2@gmail.com>
2023-05-25 20:44:24 -0400

    Create DualReceiver struct

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,41 @@ where
     }
 }
 
+pub struct DualReceiver<T, U> {
+    rx0: T,
+    rx1: U,
+    waker: &'static AtomicWaker,
+}
+
+impl<T, U> DualReceiver<T, U> {
+    pub const fn new(rx0: T, rx1: U, waker: &'static AtomicWaker) -> Self {
+        Self { rx0, rx1, waker }
+    }
+}
+
+impl<T, U> Stream for DualReceiver<T, U>
+where
+    T: Receive + Unpin,
+    U: Receive<Frame = T::Frame, Error = T::Error> + Unpin,
+{
+    type Item = Result<T::Frame, T::Error>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        match self.rx0.receive() {
+            Ok(frame) => Poll::Ready(Some(Ok(frame))),
+            Err(nb::Error::WouldBlock) => match self.rx1.receive() {
+                Ok(frame) => Poll::Ready(Some(Ok(frame))),
+                Err(nb::Error::WouldBlock) => {
+                    self.waker.register(cx.waker());
+                    Poll::Pending
+                }
+                Err(nb::Error::Other(error)) => Poll::Ready(Some(Err(error))),
+            },
+            Err(nb::Error::Other(error)) => Poll::Ready(Some(Err(error))),
+        }
+    }
+}
+
 pub trait Transmit {
     type Frame: Frame;
     type Error;
