6c06a9d8f4e8ca71a2f74e4e73b6ae1c5118e9cd
Matt Hunzinger <matthunz2@gmail.com>
2023-05-26 19:59:06 -0400

    Create Interrupt struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![cfg_attr(not(feature = "mock"), no_std)]
 
 //! Async hardware abstraction layer for embedded devices
-use core::task::Waker;
-use futures::task::AtomicWaker;
+use core::{
+    pin::Pin,
+    task::{Context, Poll, Waker},
+};
+use futures::{task::AtomicWaker, Stream};
 
 /// CAN bus
 pub mod can;
@@ -27,5 +30,37 @@ impl Scheduler for AtomicWaker {
 impl<T: Scheduler> Scheduler for &'_ T {
     fn schedule(&self, waker: &Waker) {
         (*self).schedule(waker)
+    }
+}
+
+pub struct Interrupt<S> {
+    scheduler: S,
+    is_waiting: bool,
+}
+
+impl<S> Interrupt<S> {
+    pub const fn new(scheduler: S) -> Self {
+        Self {
+            scheduler,
+            is_waiting: false,
+        }
+    }
+}
+
+impl<S> Stream for Interrupt<S>
+where
+    S: Scheduler + Unpin,
+{
+    type Item = ();
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        if self.is_waiting {
+            self.is_waiting = false;
+            Poll::Ready(Some(()))
+        } else {
+            self.is_waiting = true;
+            self.scheduler.schedule(cx.waker());
+            Poll::Pending
+        }
     }
 }
