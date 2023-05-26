2f193f349a43e2fd5f94384f437a5159e91c06f6
Matt Hunzinger <matthunz2@gmail.com>
2023-05-26 15:00:25 -0400

    Add Executor::run method

--- src/executor.rs
+++ src/executor.rs
@@ -3,11 +3,16 @@ use core::{
     cell::UnsafeCell,
     mem::MaybeUninit,
     sync::atomic::{AtomicUsize, Ordering},
+    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
 };
+use futures::{Future, FutureExt};
+
+static READY: AtomicUsize = AtomicUsize::new(0);
 
 pub struct Executor<T, const N: usize> {
     tasks: [UnsafeCell<MaybeUninit<T>>; N],
     set: AtomicUsize,
+    locked: AtomicUsize,
 }
 
 impl<T, const N: usize> Executor<T, N> {
@@ -16,6 +21,7 @@ impl<T, const N: usize> Executor<T, N> {
         Self {
             tasks,
             set: AtomicUsize::new((1 << N) - 1),
+            locked: AtomicUsize::new(0),
         }
     }
 
@@ -27,12 +33,56 @@ impl<T, const N: usize> Executor<T, N> {
             return Some(task);
         }
 
+        self.locked.fetch_or(1 << idx, Ordering::SeqCst);
+
         let cell = unsafe { &mut *self.tasks[idx].get() };
         *cell = MaybeUninit::new(task);
 
         let mask = !(1 << idx);
         self.set.fetch_and(mask, Ordering::SeqCst);
+        self.locked.fetch_and(mask, Ordering::SeqCst);
+
+        READY.fetch_or(1 << idx, Ordering::SeqCst);
 
         None
+    }
+
+    // TODO this is unsafe if multiple interrupts use `run`.
+    pub fn run(&self) -> Option<T::Output>
+    where
+        T: Future + Unpin,
+    {
+        loop {
+            let ready = READY.load(Ordering::SeqCst);
+
+            if ready == 0 {
+                break None;
+            }
+
+            let idx = ready.trailing_zeros() as usize;
+            if (self.locked.load(Ordering::SeqCst) & (1 << idx)) != 0 {
+                continue;
+            }
+
+            let mask = !(1 << idx);
+            READY.fetch_and(mask, Ordering::SeqCst);
+
+            let cell = unsafe { &mut *self.tasks[idx].get() };
+            let task = unsafe { cell.assume_init_mut() };
+
+            static VTABLE: RawWakerVTable =
+                RawWakerVTable::new(|_| todo!(), |_| {}, |_| {}, |_| {});
+            let raw_waker = RawWaker::new(&(), &VTABLE);
+            let waker = unsafe { Waker::from_raw(raw_waker) };
+            let mut cx = Context::from_waker(&waker);
+
+            if let Poll::Ready(output) = task.poll_unpin(&mut cx) {
+                *cell = MaybeUninit::uninit();
+                let mask = !(1 << idx);
+                self.set.fetch_and(mask, Ordering::SeqCst);
+
+                break Some(output);
+            }
+        }
     }
 }
--- tests/executor.rs
+++ tests/executor.rs
@@ -1,4 +1,7 @@
+use std::pin::Pin;
+
 use async_hal::executor::Executor;
+use futures::Future;
 
 #[test]
 fn it_spawns() {
@@ -7,4 +10,23 @@ fn it_spawns() {
     assert!(executor.spawn(()).is_none());
 
     assert!(executor.spawn(()).is_some());
+}
+
+#[test]
+fn it_runs() {
+    let executor = Executor::<_, 2>::new();
+
+    let a: Pin<Box<dyn Future<Output = ()>>> = Box::pin(async {
+        dbg!("A");
+    });
+    let b: Pin<Box<dyn Future<Output = ()>>> = Box::pin(async {
+        dbg!("B");
+    });
+
+    executor.spawn(a);
+    executor.spawn(b);
+
+    assert!(executor.run().is_some());
+    assert!(executor.run().is_some());
+    assert!(executor.run().is_none());
 }
