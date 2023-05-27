ff0cd87ad6e7fcea6bac1555e2b3efe193703976
Matt Hunzinger <matthunz2@gmail.com>
2023-05-27 17:42:03 -0400

    Fix example

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -1,63 +1,30 @@
-//! blinky timer using interrupts on TIM2
-//!
-//! This assumes that a LED is connected to pc13 as is the case on the blue pill board.
-//!
-//! Please note according to RM0008:
-//! "Due to the fact that the switch only sinks a limited amount of current (3 mA), the use of
-//! GPIOs PC13 to PC15 in output mode is restricted: the speed has to be limited to 2MHz with
-//! a maximum load of 30pF and these IOs must not be used as a current source (e.g. to drive a LED)"
-
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
-
-use stm32f1xx_hal as hal;
-
 use crate::hal::{
-    gpio::{gpioc, Output, PushPull},
     pac::{interrupt, Interrupt, Peripherals, TIM2},
     prelude::*,
-    timer::{CounterMs, Event},
+    timer::Event,
 };
-
-use core::cell::RefCell;
-use cortex_m::{asm::{wfi, wfe}, interrupt::Mutex, peripheral::NVIC};
+use async_hal::{Executor, Timer};
+use cortex_m::asm::wfi;
 use cortex_m_rt::entry;
+use futures::task::AtomicWaker;
+use panic_halt as _;
+use pin_utils::pin_mut;
+use stm32f1xx_hal as hal;
 
-// NOTE You can uncomment 'hprintln' here and in the code below for a bit more
-// verbosity at runtime, at the cost of throwing off the timing of the blink
-// (using 'semihosting' for printing debug info anywhere slows program
-// execution down)
-//use cortex_m_semihosting::hprintln;
-
-// A type definition for the GPIO pin to be used for our LED
-type LedPin = gpioc::PC13<Output<PushPull>>;
-
-// Make LED pin globally available
-static G_LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
-
-
-// Define an interupt handler, i.e. function to call when interrupt occurs.
-// This specific interrupt will "trip" when the timer TIM2 times out
 #[interrupt]
 fn TIM2() {
-    static mut LED: Option<LedPin> = None;
-    static mut TIM: Option<CounterMs<TIM2>> = None;
-
-    let led = LED.get_or_insert_with(|| {
-        cortex_m::interrupt::free(|cs| {
-            // Move LED pin here, leaving a None in its place
-            G_LED.borrow(cs).replace(None).unwrap()
-        })
-    });
-
-
-
-    let _ = led.toggle();
-
-    NVIC::unpend(Interrupt::TIM2);
+    TIMER_WAKER.wake();
+    unsafe {
+        (*TIM2::ptr())
+            .sr
+            .write(|w| w.bits(0xffff & !Event::Update.bits()));
+    };
 }
+
+static TIMER_WAKER: AtomicWaker = AtomicWaker::new();
 
 #[entry]
 fn main() -> ! {
@@ -74,25 +41,29 @@ fn main() -> ! {
     // Configure PC13 pin to blink LED
     let mut gpioc = dp.GPIOC.split();
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    let _ = led.set_high(); // Turn off
 
-    // Move the pin into our global storage
-    cortex_m::interrupt::free(|cs| *G_LED.borrow(cs).borrow_mut() = Some(led));
-
-    // Set up a timer expiring after 1s
     let mut timer = dp.TIM2.counter_ms(&clocks);
-    timer.start(1.secs()).unwrap();
-
-    // Generate an interrupt when the timer expires
     timer.listen(Event::Update);
+    let mut timer = Timer::new(timer, &TIMER_WAKER);
 
-  
+    let task = async {
+        loop {
+            led.toggle();
+            timer.wait(1.secs()).await;
+        }
+    };
+    pin_mut!(task);
+
+    let mut executor = Executor::<_, 1>::take().unwrap();
+    executor.spawn(task);
+
     unsafe {
         cortex_m::peripheral::NVIC::unmask(Interrupt::TIM2);
     }
 
     loop {
-        wfe();
-        let _ = timer.wait();
+        while executor.run().is_some() {}
+
+        wfi();
     }
 }
