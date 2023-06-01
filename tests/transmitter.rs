6354cc2e06d787b53ff016c6bd03dbdf0f743cf3
Matt Hunzinger <matthunz2@gmail.com>
2023-06-01 01:36:35 -0400

    Replace default features with full flag

--- tests/transmitter.rs
+++ tests/transmitter.rs
@@ -1,17 +1,20 @@
-use async_hal::can::{MockFrame, MockTransmit, Transmitter};
-use embedded_hal::can::{Id, StandardId};
-use futures::{task::AtomicWaker, SinkExt};
+#[cfg(feature = "mock")]
+mod tests {
+    use async_hal::can::{MockFrame, MockTransmit, Transmitter};
+    use embedded_hal::can::{Id, StandardId};
+    use futures::{task::AtomicWaker, SinkExt};
 
-#[tokio::test]
-async fn it_works() {
-    let waker = AtomicWaker::new();
-    let mut tx = Transmitter::new(MockTransmit::default(), &waker);
+    #[tokio::test]
+    async fn it_works() {
+        let waker = AtomicWaker::new();
+        let mut tx = Transmitter::new(MockTransmit::default(), &waker);
 
-    let frame = MockFrame {
-        id: Id::Standard(StandardId::ZERO),
-        data: vec![1, 2, 3],
-    };
-    tx.send(frame.clone()).await.unwrap();
+        let frame = MockFrame {
+            id: Id::Standard(StandardId::ZERO),
+            data: vec![1, 2, 3],
+        };
+        tx.send(frame.clone()).await.unwrap();
 
-    assert_eq!(tx.transmit.frames[0], frame);
+        assert_eq!(tx.transmit.frames[0], frame);
+    }
 }
